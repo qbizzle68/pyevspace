6cac84ac8075dbc111d47c83461bece686d0757b
qbarnes <quinton.barnes68@gmail.com>
2022-04-25 13:50:08 -0500

    finished implimenting C API

--- pyevspace/include/evspacemodule.h
+++ pyevspace/include/evspacemodule.h
@@ -3,52 +3,98 @@
 
 #ifdef __cplusplus
 extern "C" {
+#else
+
+#ifndef _BOOL
+typedef unsigned char bool;
+static const bool False = 0;
+static const bool True = 1;
 #endif
 
-#define EVSpace_BINARY (const EVector* lhs, const EVector* rhs)
+#endif
 
 /*	nb_add	*/
 #define EVSpace_Add_NUM 0
-#define EVSpace_Add_RETURN EVector*
-#define EVSpace_Add_PROTO (const EVector* lhs, const EVector* rhs)
+#define EVSpace_Add_RETURN void
+#define EVSpace_Add_PROTO (EVector* ans, const EVector* lhs, const EVector* rhs)
 /*	nb_subtract	 */
 #define EVSpace_Sub_NUM 1
-#define EVSpace_Sub_RETURN EVector*
-#define EVSpace_Sub_PROTO EVSpace_BINARY
+#define EVSpace_Sub_RETURN void
+#define EVSpace_Sub_PROTO (EVector* ans, const EVector* lhs, const EVector* rhs)
 /*	nb_mult  */
 #define EVSpace_Mult_NUM 2
-#define EVSpace_Mult_RETURN EVector*
-#define EVSpace_Mult_PROTO EVSpace_BINARY
+#define EVSpace_Mult_RETURN void
+#define EVSpace_Mult_PROTO (EVector* ans, const EVector* lhs, const EVector* rhs)
 /*	nb_negative  */
 #define EVSpace_Neg_NUM 3
-#define EVSpace_Neg_RETURN EVector*
-#define EVSpace_Neg_PROTO (const EVector* vec)
+#define EVSpace_Neg_RETURN void
+#define EVSpace_Neg_PROTO (EVector* ans, const EVector* vec)
 /*	nb_absolute  */		// magnitude ?
 #define EVSpace_Abs_NUM 4
 #define EVSpace_Abs_RETURN double
 #define EVSpace_Abs_PROTO (const EVector* vec)
 /*	nb_inplace_add  */
 #define EVSpace_Iadd_NUM 5
-#define EVSpace_Iadd_RETURN EVector*
-#define EVSpace_Iadd_PROTO EVSpace_BINARY
+#define EVSpace_Iadd_RETURN void
+#define EVSpace_Iadd_PROTO (EVector* lhs, const EVector* rhs)
 /*	nb_inplace_subtract  */
 #define EVSpace_Isub_NUM 6
-#define EVSpace_Isub_RETURN EVector*
-#define EVSpace_Isub_PROTO EVSpace_BINARY
+#define EVSpace_Isub_RETURN void
+#define EVSpace_Isub_PROTO (EVector* lhs, const EVector* rhs)
 /*	nb_inplace_multiply  */
 #define EVSpace_Imult_NUM 7
-#define EVSpace_Imult_RETURN EVector*
-#define EVSpace_Imult_PROTO EVSpace_BINARY
+#define EVSpace_Imult_RETURN void
+#define EVSpace_Imult_PROTO (EVector* lhs, const EVector* rhs)
 /*	nb_true_divide  */
 #define EVSpace_Div_NUM 8
-#define EVSpace_Div_RETURN EVector*
-#define EVSpace_Div_PROTO (const EVector* lhs, double rhs)
+#define EVSpace_Div_RETURN void
+#define EVSpace_Div_PROTO (EVector* ans, const EVector* lhs, double rhs)
 /*	nb_inplace_true_divide  */
 #define EVSpace_Idiv_NUM 9
-#define EVSpace_Idiv_RETURN EVector*
-#define EVSpace_Idiv_PROTO (const EVector* lhs, double rhs)
+#define EVSpace_Idiv_RETURN void
+#define EVSpace_Idiv_PROTO (EVector* lhs, double rhs)
+/*  equal to  */
+#define EVSpace_ET_NUM 10
+#define EVSpace_ET_RETURN bool
+#define EVSpace_ET_PROTO (const EVector* lhs, const EVector* rhs)
+/*  not equal to  */
+#define EVSpace_NE_NUM 11
+#define EVSpace_NE_RETURN bool
+#define EVSpace_NE_PROTO (const EVector* lhs, const EVector* rhs)
+/*  dot  */
+#define EVSpace_Dot_NUM 12
+#define EVSpace_Dot_RETURN double
+#define EVSpace_Dot_PROTO (const EVector* lhs, const EVector* rhs)
+/*  cross  */
+#define EVSpace_Cross_NUM 13
+#define EVSpace_Cross_RETURN void
+#define EVSpace_Cross_PROTO (EVector* and, const EVector* lhs, const EVector* rhs)
+/*  magnitude  */
+#define EVSpace_Mag_NUM 14
+#define EVSpace_Mag_RETURN double
+#define EVSpace_Mag_PROTO (const EVector* vec)
+/*  magnitude squared  */
+#define EVSpace_Mag2_NUM 15
+#define EVSpace_Mag2_RETURN double
+#define EVSpace_Mag2_PROTO (const EVector* vec)
+/*  norm  */
+#define EVSpace_Norm_NUM 16
+#define EVSpace_Norm_RETURN void
+#define EVSpace_Norm_PROTO (EVector* ans, const EVector* vec)
+/*  inplace norm  */
+#define EVSpace_Inorm_NUM 17
+#define EVSpace_Inorm_RETURN void
+#define EVSpace_Inorm_PROTO (EVector* ans)
+/*  vector angle  */
+#define EVSpace_Vang_NUM 18
+#define EVSpace_Vang_RETURN double
+#define EVSpace_Vang_PROTO (const EVector* lhs, const EVector* rhs)
+/*  vector exclude  */
+#define EVSpace_Vxcl_NUM 19
+#define EVSpace_Vxcl_RETURN void
+#define EVSpace_Vxcl_PROTO (EVector* ans, const EVector* vec, const EVector* xcl)
 
-#define EVSpace_API_pointers 9
+#define EVSpace_API_pointers 20
 
 #ifdef EVSPACE_MODULE
 	static EVSpace_Add_RETURN EVector_Add EVSpace_Add_PROTO;
@@ -61,8 +107,18 @@ extern "C" {
 	static EVSpace_Imult_RETURN EVector_Imult EVSpace_Imult_PROTO;
 	static EVSpace_Div_RETURN EVector_Div EVSpace_Div_PROTO;
 	static EVSpace_Idiv_RETURN EVector_Idiv EVSpace_Idiv_PROTO;
+	static EVSpace_ET_RETURN EVector_ET EVSpace_ET_PROTO;
+	static EVSpace_NE_RETURN EVector_NE EVSpace_Neg_PROTO;
+	static EVSpace_Dot_RETURN EVector_Dot EVSpace_Dot_PROTO;
+	static EVSpace_Cross_RETURN EVector_Cross EVSpace_Cross_PROTO;
+	static EVSpace_Mag_RETURN EVector_Mag EVSpace_Mag_PROTO;
+	static EVSpace_Mag2_RETURN EVector_Mag2 EVSpace_Mag2_PROTO;
+	static EVSpace_Norm_RETURN EVector_Norm EVSpace_Norm_PROTO;
+	static EVSpace_Inorm_RETURN EVector_Inorm EVSpace_Inorm_PROTO;
+	static EVSpace_Vang_RETURN EVector_Vang EVSpace_Vang_PROTO;
+	static EVSpace_Vxcl_RETURN EVector_Vxcl EVSpace_Vxcl_PROTO;
 #else
-	static void** EVector_API;
+	static void** EVSpace_API;
 
 #define EVSpace_Add \
 	(*(EVSpace_Add_RETURN) (*)EVSpace_Add_PROTO) EVSpace_API[EVSpace_Add_NUM]
@@ -84,6 +140,26 @@ extern "C" {
 	(*(EVSpace_Div_RETURN) (*)EVSpace_Div_PROTO) EVSpace_API[EVSpace_Div_NUM]
 #define EVSpace_Idiv \
 	(*(EVSpace_Idiv_RETURN) (*)EVSpace_Idiv_PROTO) EVSpace_API[EVSpace_Idiv_NUM]
+#define EVSpace_ET \
+	(*(EVSpace_ET_RETURN) (*)EVSpace_ET_PROTO) EVSpace_API[EVSpace_ET_NUM]
+#define EVSpace_NE \
+	(*(EVSpace_NE_RETURN) (*)EVSpace_NE_PROTO) EVSpace_API[EVSpace_NE_NUM]
+#define EVSpace_Dot \
+	(*(EVSpace_Dot_RETURN) (*)EVSpace_Dot_PROTO) EVSpace_API[EVSpace_Dot_NUM]
+#define EVSpace_Cross \
+	(*(EVSpace_Cross_RETURN) (*)EVSpace_Cross_PROTO) EVSpace_API[EVSpace_Cross_NUM]
+#define EVSpace_Mag \
+	(*(EVSpace_Mag_RETURN) (*)EVSpace_Mag_PROTO) EVSpace_API[EVSpace_Mag_NUM]
+#define EVSpace_Mag2 \
+	(*(EVSpace_Mag2_RETURN) (*)EVSpace_Mag2_PROTO) EVSpace_API[EVSpace_Mag2_NUM]
+#define EVSpace_Norm \
+	(*(EVSpace_Norm_RETURN) (*)EVSpace_Norm_PROTO) EVSpace_API[EVSpace_Norm_NUM]
+#define EVSpace_Inorm \
+	(*(EVSpace_Inorm_RETURN) (*)EVSpace_Inorm_PROTO) EVSpace_API[EVSpace_Inorm_NUM]
+#define EVSpace_Vang \
+	(*(EVSpace_Vang_RETURN) (*)EVSpace_Vang_PROTO) EVSpace_API[EVSpace_Vang_NUM]
+#define EVSpace_Vxcl \
+	(*(EVSpace_Vxcl_RETURN) (*)EVSpace_Vxcl_PROTO) EVSpace_API[EVSpace_Vxcl_NUM]
 
 	static int import_evspace(void) 
 	{
--- pyevspace/src/evspacemodule.c
+++ pyevspace/src/evspacemodule.c
@@ -1,1 +1,272 @@
 #define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+typedef struct {
+	PyObject_HEAD
+	double m_arr[3];
+} EVector;
+
+#define EVSPACE_MODULE
+#include <evspacemodule.h>
+
+#define EVSpace_RADIANS_TO_DEGREES 180.0 / 3.14159265358979323846
+
+/*	implimentation of C API methods  */
+
+static void EVSpace_Add(EVector* rtn, const EVector* lhs, const EVector* rhs)
+{
+	rtn->m_arr[0] = lhs->m_arr[0] + rhs->m_arr[0];
+	rtn->m_arr[1] = lhs->m_arr[1] + rhs->m_arr[1];
+	rtn->m_arr[2] = lhs->m_arr[2] + rhs->m_arr[2];
+}
+
+static void EVSpace_Sub(EVector* rtn, const EVector* lhs, const EVector* rhs)
+{
+	rtn->m_arr[0] = lhs->m_arr[0] - rhs->m_arr[0];
+	rtn->m_arr[1] = lhs->m_arr[1] - rhs->m_arr[1];
+	rtn->m_arr[2] = lhs->m_arr[2] - rhs->m_arr[2];
+}
+
+static void EVSpace_Mult(EVector* rtn, const EVector* lhs, double rhs)
+{
+	rtn->m_arr[0] = lhs->m_arr[0] * rhs;
+	rtn->m_arr[1] = lhs->m_arr[1] * rhs;
+	rtn->m_arr[2] = lhs->m_arr[2] * rhs;
+}
+
+static void EVSpace_Neg(EVector* rtn, const EVector* lhs)
+{
+	rtn->m_arr[0] = -lhs->m_arr[0];
+	rtn->m_arr[1] = -lhs->m_arr[1];
+	rtn->m_arr[2] = -lhs->m_arr[2];
+}
+
+static double EVSpace_Abs(const EVector* vec)
+{
+	double mag2 = vec->m_arr[0] * vec->m_arr[0]
+		+ vec->m_arr[1] * vec->m_arr[1]
+		+ vec->m_arr[2] * vec->m_arr[2];
+
+	return sqrt(mag2);
+}
+
+static void EVSpace_Iadd(EVector* lhs, const EVector* rhs)
+{
+	lhs->m_arr[0] += rhs->m_arr[0];
+	lhs->m_arr[1] += rhs->m_arr[1];
+	lhs->m_arr[2] += rhs->m_arr[2];
+}
+
+static void EVSpace_Isub(EVector* lhs, const EVector* rhs)
+{
+	lhs->m_arr[0] -= rhs->m_arr[0];
+	lhs->m_arr[1] -= rhs->m_arr[1];
+	lhs->m_arr[2] -= rhs->m_arr[2];
+}
+
+static void EVSpace_Imult(EVector* lhs, const EVector* rhs)
+{
+	lhs->m_arr[0] *= rhs->m_arr[0];
+	lhs->m_arr[1] *= rhs->m_arr[1];
+	lhs->m_arr[2] *= rhs->m_arr[2];
+}
+
+static void EVSpace_Div(EVector* rtn, const EVector* lhs, double rhs)
+{
+	rtn->m_arr[0] = lhs->m_arr[0] / rhs;
+	rtn->m_arr[1] = lhs->m_arr[1] / rhs;
+	rtn->m_arr[2] = lhs->m_arr[2] / rhs;
+}
+
+static void EVSpace_Idiv(EVector* lhs, const EVector* rhs)
+{
+	lhs->m_arr[0] /= rhs->m_arr[0];
+	lhs->m_arr[1] /= rhs->m_arr[1];
+	lhs->m_arr[2] /= rhs->m_arr[2];
+}
+
+static bool EVSpace_ET(const EVector* lhs, const EVector* rhs)
+{
+	return (lhs->m_arr[0] == rhs->m_arr[0])
+		&& (lhs->m_arr[1] == rhs->m_arr[1])
+		&& (lhs->m_arr[2] == rhs->m_arr[2]);
+}
+
+static bool EVSpace_NE(const EVector* lhs, const EVector* rhs)
+{
+	return (lhs->m_arr[0] == rhs->m_arr[0])
+		&& (lhs->m_arr[1] == rhs->m_arr[1])
+		&& (lhs->m_arr[2] == rhs->m_arr[2]);
+}
+
+static double EVSpace_Dot(const EVector* lhs, const EVector* rhs)
+{
+	return (lhs->m_arr[0] * rhs->m_arr[0])
+		+ (lhs->m_arr[1] * rhs->m_arr[1])
+		+ (lhs->m_arr[2] * rhs->m_arr[2]);
+}
+
+static void EVSpace_Cross(EVector* rtn, const EVector* lhs, const EVector* rhs)
+{
+	rtn->m_arr[0] = ((lhs->m_arr[1] * rhs->m_arr[2]) - (lhs->m_arr[2] * rhs->m_arr[1]));
+	rtn->m_arr[1] = ((lhs->m_arr[0] * rhs->m_arr[2]) - (lhs->m_arr[2] * rhs->m_arr[0]));
+	rtn->m_arr[2] = ((lhs->m_arr[0] * rhs->m_arr[1]) - (lhs->m_arr[1] * rhs->m_arr[0]));
+}
+
+static double EVSpace_Mag(const EVector* vec)
+{
+	return sqrt(EVSpace_Dot(vec, vec));
+}
+
+static double EVSpace_Mag2(const EVector* vec)
+{
+	return EVSpace_Dot(vec, vec);
+}
+
+static void EVSpace_Norm(EVector* rtn, const EVector* vec)
+{
+	double mag = sqrt(EVSpace_Dot(vec, vec));
+	rtn->m_arr[0] = vec->m_arr[0] / mag;
+	rtn->m_arr[1] = vec->m_arr[1] / mag;
+	rtn->m_arr[2] = vec->m_arr[2] / mag;
+}
+
+static void EVSpace_Inorm(EVector* rtn)
+{
+	double mag = sqrt(EVSpace_Dot(rtn, rtn));
+	rtn->m_arr[0] /= mag;
+	rtn->m_arr[1] /= mag;
+	rtn->m_arr[2] /= mag;
+}
+
+static double EVSpace_Vang(const EVector* lhs, const EVector* rhs)
+{
+	double theta = acos(dot(lhs, rhs) / (EVSpace_Mag(lhs) * EVSpace_Mag(rhs)));
+	return theta * EVSpace_RADIANS_TO_DEGREES;
+}
+
+static void EVSpace_Vxcl(EVector* ans, const EVector* vec, const EVector* xcl)
+{
+	double scale = EVSpace_Dot(vec, xcl) / EVSpace_Mag2(xcl);
+	ans->m_arr[0] = vec->m_arr[0] - (xcl->m_arr[0] * scale);
+	ans->m_arr[1] = vec->m_arr[1] - (xcl->m_arr[1] * scale);
+	ans->m_arr[2] = vec->m_arr[2] - (xcl->m_arr[2] * scale);
+}
+
+static int EVector_init(EVector* self, PyObject* args, PyObject* kwds)
+{
+	double x = 0, y = 0, z = 0;
+
+	static char* kwlist[] = { "x", "y", "z", NULL };
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "|ddd", kwlist, &x, &y, &z))
+		return -1;
+
+	self->m_arr[0] = x;
+	self->m_arr[1] = y;
+	self->m_arr[2] = z;
+
+	return 0;
+}
+
+static PyObject* EVector_str(const EVector* vec)
+{
+	char buffer[50]; // todo: find the accurate number here
+	int ok = snprintf(buffer, 50, "[ %f, %f, %f ]", vec->m_arr[0], vec->m_arr[1], vec->m_arr[2]);
+
+	if (ok < 0 || ok > 50)
+		return NULL; // todo: do we raise an exception here?
+
+	return Py_BuildValue("s#", buffer, strlen(buffer));
+}
+
+static PyObject* Operator_Add(EVector* self, PyObject* arg)
+{
+	PyTypeObject* type = self->ob_base.ob_type;
+	Py_INCREF(type);
+	EVector* rtn = (EVector*)type->tp_new(type, NULL, NULL);
+	Py_INCREF(rtn);
+	Py_DECREF(type);
+
+	if (!rtn) {
+		Py_DECREF(rtn);
+		return NULL;
+	}
+
+	EVector_Add(rtn, self, (EVector*)arg);
+
+	return (PyObject*)rtn;
+}
+
+static PyObject* Operator_Sub(EVector* self, PyObject* arg)
+{
+	PyTypeObject* type = self->ob_base.ob_type;
+	Py_INCREF(type);
+	EVector* rtn = (EVector*)type->tp_new(type, NULL, NULL);
+	Py_INCREF(rtn);
+	Py_DECREF(type);
+
+	if (!rtn) {
+		Py_DECREF(rtn);
+		return NULL;
+	}
+
+	EVector_Sub(rtn, self, (EVector*)arg);
+
+	return (PyObject*)rtn;
+}
+
+static PyObject* Operator_Mult(EVector* self, PyObject* arg)
+{
+	PyTypeObject* type = self->ob_base.ob_type;
+	EVector* rtn = (EVector*)type->tp_new(type, NULL, NULL);
+	Py_INCREF(rtn);
+
+	if (!rtn) {
+		Py_DECREF(rtn);
+		return NULL;
+	}
+
+	EVector_Mult(rtn, self, (EVeqqctor*)arg);
+
+	return (PyObject*)rtn;
+}
+
+static PyNumberMethods EVector_NBMethods =
+{
+	Operator_Add,	/*  nb_add  */
+	Operator_Sub,	/*  nb_sub  */
+	Operator_Mult,	/*  nb_mult  */
+	0,				/*  nb_remainder  */
+	0,				/*  nb_divmod  */
+	0,				/*  nb_power  */
+	Operator_Neg,	/*  nb_negative  */
+	0,				/*  nb_positive  */
+	Operator_Abs	/*  nb_absolute  */
+	0,				/*  nb_bool  */
+	0,				/*  nb_invert  */
+	0,				/*  nb_lshift  */
+	0,				/*  nb_rshift  */
+	0,				/*  nb_and  */
+	0,				/*  nb_xor  */
+	0,				/*  nb_or  */
+	0,				/*  nb_int  */
+	0,				/*  nb_reserved  */
+	0,				/*  nb_float  */
+	Opeartor_Iadd,	/*  nb_inplace_add  */
+	Operator_Isub,	/*  nb_inplace_subtract  */
+	Operator_Imult,	/*  nb_inplace_multiply  */
+	0,				/*  nb_inplace_remainder  */
+	0,				/*  nb_inplace_power  */
+	0,				/*  nb_inplace_lshift  */
+	0,				/*  nb_inplace_rshift  */
+	0,				/*  nb_inplace_and  */
+	0,				/*  nb_inplace_xor  */
+	0,				/*  nb_inplace_or  */
+	0,				/*  nb_floor_divide  */
+	Operator_Div,	/*  nb_true_divide  */
+	0,				/*  nb_inplace_floor_divide  */
+	Operator_IDiv,	/*  nb_inplace_true_divide  */
+	0,				/*  nb_index  */
+	0,				/*  nb_matrix_multiply  */
+	0				/*  nb_inplace_matrix_multiply  */
+};
