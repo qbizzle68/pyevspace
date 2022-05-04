0a1417f1fe05b85953fcee25a86a851e59a0f388
qbarnes <quinton.barnes68@gmail.com>
2022-05-03 23:59:19 -0500

    added to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup, find_packages, Extension
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name = 'pyevspace',
     version = '0.0.1',
+    author="Quinton Barnes",
+    author_email="devqbizzle68@gmail.com",
+    description="A Euclidean vector space module."
     package = find_packages(),
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    url="https://github.com/qbizzle68/pyevspace",
     license = 'MIT',
     classifiers = [
         'License :: OSI Approved :: MIT License',
@@ -17,4 +26,5 @@ setup(
             sources = ['pyevspace/src/evspacemodule.c'],
         ),
     ],
+    python_requires=">=3.6",
 )
