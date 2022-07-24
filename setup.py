e7b15e6cb48cda62af99bb33eb073e997b5dcbf3
qbarnes <quinton.barnes68@gmail.com>
2022-07-24 00:55:38 -0500

    updated version number

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name = 'pyevspace',
-    version = '0.0.2',
+    version = '0.0.4',
     author="Quinton Barnes",
     author_email="devqbizzle68@gmail.com",
     description="A Euclidean vector space module.",
-    package = find_packages(),
+    package = find_packages(''),
     long_description = long_description,
     long_description_content_type = 'text/markdown',
     url="https://github.com/qbizzle68/pyevspace",
