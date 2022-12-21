907398d4d00746b29fe9cc5bae2103ff72fbdf22
qbarnes <quinton.barnes68@gmail.com>
2022-12-20 23:04:46 -0600

    created answers for reference frame from to

--- pyevspace/tests/rotation_answers.py
+++ pyevspace/tests/rotation_answers.py
@@ -1,29 +1,29 @@
 from math import sin, cos, pi
 from pyevspace import Matrix, Vector, X_AXIS, Y_AXIS, Z_AXIS, XYZ, XZY, YXZ, YZX, ZXY, ZYX, XYX, XZX, \
-    YXY, YZY, ZXZ, ZYZ
+    YXY, YZY, ZXZ, ZYZ, Order
 
-so = cos(pi / 4)    # cos and sin of forty-five
+so = cos(pi / 4)  # cos and sin of forty-five
 
 rotation_xaxis_answers = [
-    Matrix((1, 0, 0),       # X-rotation, 0.0 radians
+    Matrix((1, 0, 0),  # X-rotation, 0.0 radians
            (0, 1, 0),
            (0, 0, 1)),
     Matrix((1, 0, 0),  # X-rotation, pi / 4 radians
            (0, so, -so),
            (0, so, so)),
-    Matrix((1, 0, 0),       # X-rotation, pi / 2 radians
+    Matrix((1, 0, 0),  # X-rotation, pi / 2 radians
            (0, 0, -1),
            (0, 1, 0)),
     Matrix((1, 0, 0),  # X-rotation, 3 pi / 4 radians
            (0, -so, -so),
            (0, so, -so)),
-    Matrix((1, 0, 0),       # X-rotation, pi radians
+    Matrix((1, 0, 0),  # X-rotation, pi radians
            (0, -1, 0),
            (0, 0, -1)),
     Matrix((1, 0, 0),  # X-rotation, 5 pi / 4 radians
            (0, -so, so),
            (0, -so, -so)),
-    Matrix((1, 0, 0),       # X-rotation, 3 pi / 2 radians
+    Matrix((1, 0, 0),  # X-rotation, 3 pi / 2 radians
            (0, 0, 1),
            (0, -1, 0)),
     Matrix((1, 0, 0),  # X-rotation, 7 pi / 4 radians
@@ -32,25 +32,25 @@ rotation_xaxis_answers = [
 ]
 
 rotation_yaxis_answers = [
-    Matrix((1, 0, 0),       # Y-rotation, 0.0 radians
+    Matrix((1, 0, 0),  # Y-rotation, 0.0 radians
            (0, 1, 0),
            (0, 0, 1)),
     Matrix((so, 0, so),  # Y-rotation, pi / 4 radians
            (0, 1, 0),
            (-so, 0, so)),
-    Matrix((0, 0, 1),       # Y-rotation, pi / 2 radians
+    Matrix((0, 0, 1),  # Y-rotation, pi / 2 radians
            (0, 1, 0),
            (-1, 0, 0)),
     Matrix((-so, 0, so),  # Y-rotation, 3 pi / 4 radians
            (0, 1, 0),
            (-so, 0, -so)),
-    Matrix((-1, 0, 0),      # Y-rotation, pi radians
+    Matrix((-1, 0, 0),  # Y-rotation, pi radians
            (0, 1, 0),
            (0, 0, -1)),
     Matrix((-so, 0, -so),  # Y-rotation, 5 pi / 4 radians
            (0, 1, 0),
            (so, 0, -so)),
-    Matrix((0, 0, -1),      # Y-rotation, 3 pi / 2 radians
+    Matrix((0, 0, -1),  # Y-rotation, 3 pi / 2 radians
            (0, 1, 0),
            (1, 0, 0)),
     Matrix((so, 0, -so),  # Y-rotation, 7 pi / 4 radians
@@ -59,25 +59,25 @@ rotation_yaxis_answers = [
 ]
 
 rotation_zaxis_answers = [
-    Matrix((1, 0, 0),       # Z-rotation, 0 radians
+    Matrix((1, 0, 0),  # Z-rotation, 0 radians
            (0, 1, 0),
            (0, 0, 1)),
     Matrix((so, -so, 0),  # Z-rotation, pi / 4 radians
            (so, so, 0),
            (0, 0, 1)),
-    Matrix((0, -1, 0),      # Z-rotation, pi / 2 radians
+    Matrix((0, -1, 0),  # Z-rotation, pi / 2 radians
            (1, 0, 0),
            (0, 0, 1)),
     Matrix((-so, -so, 0),  # Z-rotation, 3 pi / 4 radians
            (so, -so, 0),
            (0, 0, 1)),
-    Matrix((-1, 0, 0),      # Z-rotation, pi radians
+    Matrix((-1, 0, 0),  # Z-rotation, pi radians
            (0, -1, 0),
            (0, 0, 1)),
     Matrix((-so, so, 0),  # Z-rotation, 5 pi / 4 radians
            (-so, -so, 0),
            (0, 0, 1)),
-    Matrix((0, 1, 0),       # Z-rotation, 3 pi / 2 radians
+    Matrix((0, 1, 0),  # Z-rotation, 3 pi / 2 radians
            (-1, 0, 0),
            (0, 0, 1)),
     Matrix((so, so, 0),  # Z-rotation, 7 pi / 4 radians
@@ -92,27 +92,27 @@ rotation_matrix_answers = {
 }
 
 half = 0.5
-ef = cos(pi/4)*sin(pi/4) * (1 + sin(pi/4))      # 0.8535534
-of = cos(pi/4)*cos(pi/4) * (1 - cos(pi/4))      # 0.1464466
+ef = cos(pi / 4) * sin(pi / 4) * (1 + sin(pi / 4))  # 0.8535534
+of = cos(pi / 4) * cos(pi / 4) * (1 - cos(pi / 4))  # 0.1464466
 
 rotation_XYZ_answers = [
-    Matrix.id,                  # 0 radians
+    Matrix.id,  # 0 radians
     Matrix((half, -half, so),  # pi / 4 radians
            (ef, of, -half),
            (of, ef, half)),
-    Matrix((0, 0, 1),           # pi / 2 radians
+    Matrix((0, 0, 1),  # pi / 2 radians
            (0, -1, 0),
            (1, 0, 0)),
     Matrix((half, half, so),  # 3 pi / 2 radians
            (-ef, of, half),
            (of, -ef, half)),
-    Matrix((1, 0, 0),           # pi radians
+    Matrix((1, 0, 0),  # pi radians
            (0, 1, 0),
            (0, 0, 1)),
     Matrix((half, -half, -so),  # 5 pi / 4 radians
            (of, ef, -half),
            (ef, of, half)),
-    Matrix((0, 0, -1),          # 3 pi / 2 radians
+    Matrix((0, 0, -1),  # 3 pi / 2 radians
            (0, 1, 0),
            (1, 0, 0)),
     Matrix((half, half, -so),  # 7 pi / 4 radians
@@ -121,21 +121,21 @@ rotation_XYZ_answers = [
 ]
 
 rotation_XZY_answers = [
-    Matrix.id,                  # 0 radians
+    Matrix.id,  # 0 radians
     Matrix((half, -so, half),  # pi / 4 radians
            (ef, half, -of),
            (-of, half, ef)),
-    Matrix((0, -1, 0),          # pi / 2 radians
+    Matrix((0, -1, 0),  # pi / 2 radians
            (1, 0, 0),
            (0, 0, 1)),
     Matrix((half, -so, -half),  # 3 pi / 4 radians
            (ef, half, of),
            (of, -half, ef)),
-    Matrix.id,                  # pi radians
+    Matrix.id,  # pi radians
     Matrix((half, so, half),  # 5 pi / 4 radians
            (of, half, -ef),
            (-ef, half, of)),
-    Matrix((0, 1, 0),          # 3 pi / 2 radians
+    Matrix((0, 1, 0),  # 3 pi / 2 radians
            (1, 0, 0),
            (0, 0, -1)),
     Matrix((half, so, -half),  # 7 pi / 4 radians
@@ -144,21 +144,21 @@ rotation_XZY_answers = [
 ]
 
 rotation_YXZ_answers = [
-    Matrix.id,                  # 0 radians
+    Matrix.id,  # 0 radians
     Matrix((ef, -of, half),  # pi / 4 radians
            (half, half, -so),
            (-of, ef, half)),
-    Matrix((1, 0, 0),           # pi / 2 radians
+    Matrix((1, 0, 0),  # pi / 2 radians
            (0, 0, -1),
            (0, 1, 0)),
     Matrix((ef, of, -half),  # 3 pi / 4 radians
            (-half, half, -so),
            (of, ef, half)),
-    Matrix.id,                  # pi radians
+    Matrix.id,  # pi radians
     Matrix((of, -ef, half),  # 5 pi / 4 radians
            (half, half, so),
            (-ef, of, half)),
-    Matrix((-1, 0, 0),          # 3 pi / 2 radians
+    Matrix((-1, 0, 0),  # 3 pi / 2 radians
            (0, 0, 1),
            (0, 1, 0)),
     Matrix((of, ef, -half),  # 7 pi / 4 radians
@@ -167,21 +167,21 @@ rotation_YXZ_answers = [
 ]
 
 rotation_YZX_answers = [
-    Matrix.id,                   # 0 radians
+    Matrix.id,  # 0 radians
     Matrix((half, of, ef),  # pi / 4 radians
            (so, half, -half),
            (-half, ef, of)),
-    Matrix((0, 1, 0),           # pi / 2 radians
+    Matrix((0, 1, 0),  # pi / 2 radians
            (1, 0, 0),
            (0, 0, -1)),
     Matrix((half, of, -ef),  # 3 pi / 4 radians
            (so, half, half),
            (half, -ef, of)),
-    Matrix.id,                  # pi radians
+    Matrix.id,  # pi radians
     Matrix((half, ef, of),  # 5 pi / 4 radians
            (-so, half, -half),
            (-half, of, ef)),
-    Matrix((0, 1, 0),          # 3 pi / 2 radians
+    Matrix((0, 1, 0),  # 3 pi / 2 radians
            (-1, 0, 0),
            (0, 0, 1)),
     Matrix((half, ef, -of),  # 7 pi / 4 radians
@@ -190,197 +190,197 @@ rotation_YZX_answers = [
 ]
 
 rotation_ZXY_answers = [
-    Matrix.id,                   # 0 radians
-    Matrix((of, -half, ef),     # pi / 4 radians
+    Matrix.id,  # 0 radians
+    Matrix((of, -half, ef),  # pi / 4 radians
            (ef, half, of),
            (-half, so, half)),
-    Matrix((-1, 0, 0),           # pi / 2 radians
+    Matrix((-1, 0, 0),  # pi / 2 radians
            (0, 0, 1),
            (0, 1, 0)),
-    Matrix((of, half, -ef),     # 3 pi / 4 radians
+    Matrix((of, half, -ef),  # 3 pi / 4 radians
            (-ef, half, of),
            (half, so, half)),
-    Matrix.id,                  # pi radians
-    Matrix((ef, -half, of),     # 5 pi / 4 radians
+    Matrix.id,  # pi radians
+    Matrix((ef, -half, of),  # 5 pi / 4 radians
            (of, half, ef),
            (-half, -so, half)),
-    Matrix((1, 0, 0),          # 3 pi / 2 radians
+    Matrix((1, 0, 0),  # 3 pi / 2 radians
            (0, 0, 1),
            (0, -1, 0)),
-    Matrix((ef, half, -of),     # 7 pi / 4 radians
+    Matrix((ef, half, -of),  # 7 pi / 4 radians
            (-of, half, ef),
            (half, -so, half))
 ]
 
 rotation_ZYX_answers = [
-    Matrix.id,                   # 0 radians
-    Matrix((half, -of, ef),     # pi / 4 radians
+    Matrix.id,  # 0 radians
+    Matrix((half, -of, ef),  # pi / 4 radians
            (half, ef, -of),
            (-so, half, half)),
-    Matrix((0, 0, 1),           # pi / 2 radians
+    Matrix((0, 0, 1),  # pi / 2 radians
            (0, 1, 0),
            (-1, 0, 0)),
-    Matrix((half, of, ef),     # 3 pi / 4 radians
+    Matrix((half, of, ef),  # 3 pi / 4 radians
            (-half, ef, of),
            (-so, -half, half)),
-    Matrix.id,                  # pi radians
-    Matrix((half, -ef, of),     # 5 pi / 4 radians
+    Matrix.id,  # pi radians
+    Matrix((half, -ef, of),  # 5 pi / 4 radians
            (half, of, -ef),
            (so, half, half)),
-    Matrix((0, 0, 1),          # 3 pi / 2 radians
+    Matrix((0, 0, 1),  # 3 pi / 2 radians
            (0, -1, 0),
            (1, 0, 0)),
-    Matrix((half, ef, of),     # 7 pi / 4 radians
+    Matrix((half, ef, of),  # 7 pi / 4 radians
            (-half, of, ef),
            (so, -half, half))
 ]
 
 rotation_XYX_answers = [
-    Matrix.id,                   # 0 radians
-    Matrix((so, half, half),     # pi / 4 radians
+    Matrix.id,  # 0 radians
+    Matrix((so, half, half),  # pi / 4 radians
            (half, of, -ef),
            (-half, ef, -of)),
-    Matrix((0, 1, 0),           # pi / 2 radians
+    Matrix((0, 1, 0),  # pi / 2 radians
            (1, 0, 0),
            (0, 0, -1)),
-    Matrix((-so, half, -half),     # 3 pi / 4 radians
+    Matrix((-so, half, -half),  # 3 pi / 4 radians
            (half, ef, of),
            (half, -of, -ef)),
-    Matrix((-1, 0, 0),                  # pi radians
+    Matrix((-1, 0, 0),  # pi radians
            (0, 1, 0),
            (0, 0, -1)),
-    Matrix((-so, half, half),     # 5 pi / 4 radians
+    Matrix((-so, half, half),  # 5 pi / 4 radians
            (half, ef, -of),
            (-half, of, -ef)),
-    Matrix((0, 1, 0),          # 3 pi / 2 radians
+    Matrix((0, 1, 0),  # 3 pi / 2 radians
            (1, 0, 0),
            (0, 0, -1)),
-    Matrix((so, half, -half),     # 7 pi / 4 radians
+    Matrix((so, half, -half),  # 7 pi / 4 radians
            (half, of, ef),
            (half, -ef, -of))
 ]
 
 rotation_XZX_answers = [
-    Matrix.id,                   # 0 radians
-    Matrix((so, -half, half),     # pi / 4 radians
+    Matrix.id,  # 0 radians
+    Matrix((so, -half, half),  # pi / 4 radians
            (half, -of, -ef),
            (half, ef, of)),
-    Matrix((0, 0, 1),           # pi / 2 radians
+    Matrix((0, 0, 1),  # pi / 2 radians
            (0, -1, 0),
            (1, 0, 0)),
-    Matrix((-so, half, half),     # 3 pi / 4 radians
+    Matrix((-so, half, half),  # 3 pi / 4 radians
            (-half, -ef, of),
            (half, -of, ef)),
-    Matrix((-1, 0, 0),                  # pi radians
+    Matrix((-1, 0, 0),  # pi radians
            (0, -1, 0),
            (0, 0, 1)),
-    Matrix((-so, -half, half),     # 5 pi / 4 radians
+    Matrix((-so, -half, half),  # 5 pi / 4 radians
            (half, -ef, -of),
            (half, of, ef)),
-    Matrix((0, 0, 1),          # 3 pi / 2 radians
+    Matrix((0, 0, 1),  # 3 pi / 2 radians
            (0, -1, 0),
            (1, 0, 0)),
-    Matrix((so, half, half),     # 7 pi / 4 radians
+    Matrix((so, half, half),  # 7 pi / 4 radians
            (-half, -of, ef),
            (half, -ef, of))
 ]
 
 rotation_YXY_answers = [
-    Matrix.id,                   # 0 radians
-    Matrix((of, half, ef),     # pi / 4 radians
+    Matrix.id,  # 0 radians
+    Matrix((of, half, ef),  # pi / 4 radians
            (half, so, -half),
            (-ef, half, -of)),
-    Matrix((0, 1, 0),           # pi / 2 radians
+    Matrix((0, 1, 0),  # pi / 2 radians
            (1, 0, 0),
            (0, 0, -1)),
-    Matrix((ef, half, -of),     # 3 pi / 4 radians
+    Matrix((ef, half, -of),  # 3 pi / 4 radians
            (half, -so, half),
            (of, -half, -ef)),
-    Matrix((1, 0, 0),                  # pi radians
+    Matrix((1, 0, 0),  # pi radians
            (0, -1, 0),
            (0, 0, -1)),
-    Matrix((ef, half, of),     # 5 pi / 4 radians
+    Matrix((ef, half, of),  # 5 pi / 4 radians
            (half, -so, -half),
            (-of, half, -ef)),
-    Matrix((0, 1, 0),          # 3 pi / 2 radians
+    Matrix((0, 1, 0),  # 3 pi / 2 radians
            (1, 0, 0),
            (0, 0, -1)),
-    Matrix((of, half, -ef),     # 7 pi / 4 radians
+    Matrix((of, half, -ef),  # 7 pi / 4 radians
            (half, so, half),
            (ef, -half, -of))
 ]
 
 rotation_YZY_answers = [
-    Matrix.id,                   # 0 radians
-    Matrix((-of, -half, ef),     # pi / 4 radians
+    Matrix.id,  # 0 radians
+    Matrix((-of, -half, ef),  # pi / 4 radians
            (half, so, half),
            (-ef, half, of)),
-    Matrix((-1, 0, 0),           # pi / 2 radians
+    Matrix((-1, 0, 0),  # pi / 2 radians
            (0, 0, 1),
            (0, 1, 0)),
-    Matrix((-ef, half, -of),     # 3 pi / 4 radians
+    Matrix((-ef, half, -of),  # 3 pi / 4 radians
            (-half, -so, half),
            (of, half, ef)),
-    Matrix((-1, 0, 0),                  # pi radians
+    Matrix((-1, 0, 0),  # pi radians
            (0, -1, 0),
            (0, 0, 1)),
-    Matrix((-ef, -half, of),     # 5 pi / 4 radians
+    Matrix((-ef, -half, of),  # 5 pi / 4 radians
            (half, -so, half),
            (-of, half, ef)),
-    Matrix((-1, 0, 0),          # 3 pi / 2 radians
+    Matrix((-1, 0, 0),  # 3 pi / 2 radians
            (0, 0, 1),
            (0, 1, 0)),
-    Matrix((-of, half, -ef),     # 7 pi / 4 radians
+    Matrix((-of, half, -ef),  # 7 pi / 4 radians
            (-half, so, half),
            (ef, half, of))
 ]
 
 rotation_ZXZ_answers = [
-    Matrix.id,                   # 0 radians
-    Matrix((of, -ef, half),     # pi / 4 radians
+    Matrix.id,  # 0 radians
+    Matrix((of, -ef, half),  # pi / 4 radians
            (ef, -of, -half),
            (half, half, so)),
-    Matrix((0, 0, 1),           # pi / 2 radians
+    Matrix((0, 0, 1),  # pi / 2 radians
            (0, -1, 0),
            (1, 0, 0)),
-    Matrix((ef, of, half),     # 3 pi / 4 radians
+    Matrix((ef, of, half),  # 3 pi / 4 radians
            (-of, -ef, half),
            (half, -half, -so)),
-    Matrix((1, 0, 0),                  # pi radians
+    Matrix((1, 0, 0),  # pi radians
            (0, -1, 0),
            (0, 0, -1)),
-    Matrix((ef, -of, half),     # 5 pi / 4 radians
+    Matrix((ef, -of, half),  # 5 pi / 4 radians
            (of, -ef, -half),
            (half, half, -so)),
-    Matrix((0, 0, 1),          # 3 pi / 2 radians
+    Matrix((0, 0, 1),  # 3 pi / 2 radians
            (0, -1, 0),
            (1, 0, 0)),
-    Matrix((of, ef, half),     # 7 pi / 4 radians
+    Matrix((of, ef, half),  # 7 pi / 4 radians
            (-ef, -of, half),
            (half, -half, so))
 ]
 
 rotation_ZYZ_answers = [
-    Matrix.id,                   # 0 radians
-    Matrix((-of, -ef, half),     # pi / 4 radians
+    Matrix.id,  # 0 radians
+    Matrix((-of, -ef, half),  # pi / 4 radians
            (ef, of, half),
            (-half, half, so)),
-    Matrix((-1, 0, 0),           # pi / 2 radians
+    Matrix((-1, 0, 0),  # pi / 2 radians
            (0, 0, 1),
            (0, 1, 0)),
-    Matrix((-ef, of, -half),     # 3 pi / 4 radians
+    Matrix((-ef, of, -half),  # 3 pi / 4 radians
            (-of, ef, half),
            (half, half, -so)),
-    Matrix((-1, 0, 0),                  # pi radians
+    Matrix((-1, 0, 0),  # pi radians
            (0, 1, 0),
            (0, 0, -1)),
-    Matrix((-ef, -of, half),     # 5 pi / 4 radians
+    Matrix((-ef, -of, half),  # 5 pi / 4 radians
            (of, ef, half),
            (-half, half, -so)),
-    Matrix((-1, 0, 0),          # 3 pi / 2 radians
+    Matrix((-1, 0, 0),  # 3 pi / 2 radians
            (0, 0, 1),
            (0, 1, 0)),
-    Matrix((-of, ef, -half),     # 7 pi / 4 radians
+    Matrix((-of, ef, -half),  # 7 pi / 4 radians
            (-ef, of, half),
            (half, half, so))
 ]
@@ -935,4 +935,50 @@ rotation_from_to_answers = {
     YZY: rotation_from_YZY_to,
     ZXZ: rotation_from_ZXZ_to,
     ZYZ: rotation_from_ZYZ_to
+}
+
+_vectors = [Vector.e1, Vector.e2, Vector.e3]
+_vectors_neg = [-Vector.e1, -Vector.e2, -Vector.e3]
+def make_dict(d):
+    rtn = {}
+    for order, matrix in d.items():
+        subd = {}
+        rows = [Vector(matrix[0]), Vector(matrix[1]), Vector(matrix[2])]
+        for i in range(3):
+            try:
+                id = rows.index(_vectors[i])
+                ls = _vectors
+            except ValueError:
+                id = rows.index(_vectors_neg[i])
+                ls = _vectors_neg
+            subd[i] = ls[id]
+        rtn[order] = subd
+    return rtn
+
+rotation_refframe_from_XYZ_to = make_dict(rotation_from_XYZ_to)
+rotation_refframe_from_XZY_to = make_dict(rotation_from_XZY_to)
+rotation_refframe_from_YXZ_to = make_dict(rotation_from_YXZ_to)
+rotation_refframe_from_YZX_to = make_dict(rotation_from_YZX_to)
+rotation_refframe_from_ZXY_to = make_dict(rotation_from_ZXY_to)
+rotation_refframe_from_ZYX_to = make_dict(rotation_from_ZYX_to)
+rotation_refframe_from_XYX_to = make_dict(rotation_from_XYX_to)
+rotation_refframe_from_XZX_to = make_dict(rotation_from_XZX_to)
+rotation_refframe_from_YXY_to = make_dict(rotation_from_YXY_to)
+rotation_refframe_from_YZY_to = make_dict(rotation_from_YZY_to)
+rotation_refframe_from_ZXZ_to = make_dict(rotation_from_ZXZ_to)
+rotation_refframe_from_ZYZ_to = make_dict(rotation_from_ZYZ_to)
+
+rotation_refframe_from_to_answers = {
+    XYZ: rotation_refframe_from_XYZ_to,
+    XZY: rotation_refframe_from_XZY_to,
+    YXZ: rotation_refframe_from_YXZ_to,
+    YZX: rotation_refframe_from_YZX_to,
+    ZXY: rotation_refframe_from_ZXY_to,
+    ZYX: rotation_refframe_from_ZYX_to,
+    XYX: rotation_refframe_from_XYX_to,
+    XZX: rotation_refframe_from_XZX_to,
+    YXY: rotation_refframe_from_YXY_to,
+    YZY: rotation_refframe_from_YZY_to,
+    ZXZ: rotation_refframe_from_ZXZ_to,
+    ZYZ: rotation_refframe_from_ZYZ_to
 }
