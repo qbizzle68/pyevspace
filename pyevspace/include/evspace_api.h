274c3e3474d2f3f96b5d80b2f341d3a7cfe3200b
qbarnes <quinton.barnes68@gmail.com>
2023-01-05 20:12:46 -0600

    fixed random tabs

--- pyevspace/include/evspace_api.h
+++ pyevspace/include/evspace_api.h
@@ -1,55 +1,51 @@
 #ifndef EVSPACE_API_H
 #define EVSPACE_API_H
 
-//#ifdef __cplusplus
-//extern "C" {
-//#endif
-
 #include <Python.h>
 
-    /* C types */
+/* C types */
 
-    typedef struct {
-        PyObject_HEAD
-        double* data;			/* x, y, z component of vectors */
-    } EVSpace_Vector;
-
-
-    typedef struct {
-        PyObject_HEAD
-        double* data;			/* row by column ordering */
-    } EVSpace_Matrix;
-
-    typedef struct {
-        PyObject_HEAD
-        double alpha;
-        double beta;
-        double gamma;
-    } EVSpace_Angles;
-
-    typedef enum {
-        X_AXIS = 0,
-        Y_AXIS = 1,
-        Z_AXIS = 2
-    } EVSpace_Axis;
-
-    typedef struct {
-        PyObject_HEAD
-        EVSpace_Axis first;
-        EVSpace_Axis second;
-        EVSpace_Axis third;
-    } EVSpace_Order;
-
-    typedef struct {
-        PyObject_HEAD
-        EVSpace_Order* order;
-        EVSpace_Angles* angles;
-        EVSpace_Matrix* matrix;
-        EVSpace_Vector* offset;
-    } EVSpace_ReferenceFrame;
+typedef struct {
+    PyObject_HEAD
+    double* data;			/* x, y, z component of vectors */
+} EVSpace_Vector;
 
 
-    /* define macros for accessing EVSpace_Vector array values */
+typedef struct {
+    PyObject_HEAD
+    double* data;			/* row by column ordering */
+} EVSpace_Matrix;
+
+typedef struct {
+    PyObject_HEAD
+    double alpha;
+    double beta;
+    double gamma;
+} EVSpace_Angles;
+
+typedef enum {
+    X_AXIS = 0,
+    Y_AXIS = 1,
+    Z_AXIS = 2
+} EVSpace_Axis;
+
+typedef struct {
+    PyObject_HEAD
+    EVSpace_Axis first;
+    EVSpace_Axis second;
+    EVSpace_Axis third;
+} EVSpace_Order;
+
+typedef struct {
+    PyObject_HEAD
+    EVSpace_Order* order;
+    EVSpace_Angles* angles;
+    EVSpace_Matrix* matrix;
+    EVSpace_Vector* offset;
+} EVSpace_ReferenceFrame;
+
+
+/* define macros for accessing EVSpace_Vector array values */
 #define EVSpace_VECTOR_X(o)		(((EVSpace_Vector*)o)->data[0])
 #define EVSpace_VECTOR_Y(o)		(((EVSpace_Vector*)o)->data[1])
 #define EVSpace_VECTOR_Z(o)		(((EVSpace_Vector*)o)->data[2])
@@ -69,110 +65,111 @@
 
 
 /* Define structure for C API */
-    typedef struct {
-        /* type objects */
-        PyTypeObject* VectorType;
-        PyTypeObject* MatrixType;
-        PyTypeObject* AnglesType;
-        PyTypeObject* OrderType;
-        PyTypeObject* RotationType;
+typedef struct {
+    /* type objects */
+    PyTypeObject* VectorType;
+    PyTypeObject* MatrixType;
+    PyTypeObject* AnglesType;
+    PyTypeObject* OrderType;
+    PyTypeObject* RotationType;
 
-        /* constructors */
-        EVSpace_Vector* (*Vector_FromArray)(double*, PyTypeObject*);
-        EVSpace_Vector* (*Vector_StealArray)(double*, PyTypeObject*);
-        EVSpace_Matrix* (*Matrix_FromArray)(double*, PyTypeObject*);
-        EVSpace_Matrix* (*Matrix_StealArray)(double*, PyTypeObject*);
-        EVSpace_Angles* (*Angles_New)(double, double, double, PyTypeObject*);
-        EVSpace_Order* (*Order_New)(EVSpace_Axis, EVSpace_Axis, EVSpace_Axis,
-            PyTypeObject*);
+    /* constructors */
+    EVSpace_Vector* (*Vector_FromArray)(double*, PyTypeObject*);
+    EVSpace_Vector* (*Vector_StealArray)(double*, PyTypeObject*);
+    EVSpace_Matrix* (*Matrix_FromArray)(double*, PyTypeObject*);
+    EVSpace_Matrix* (*Matrix_StealArray)(double*, PyTypeObject*);
+    EVSpace_Angles* (*Angles_New)(double, double, double, PyTypeObject*);
+    EVSpace_Order* (*Order_New)(EVSpace_Axis, EVSpace_Axis, EVSpace_Axis,
+        PyTypeObject*);
 
-        /* vector number methods */
-        EVSpace_Vector* (*EVSpace_Vector_add)(const EVSpace_Vector*, const EVSpace_Vector*);
-        EVSpace_Vector* (*EVSpace_Vector_subtract)(const EVSpace_Vector*,
-            const EVSpace_Vector*);
-        EVSpace_Vector* (*EVSpace_Vector_multiply)(const EVSpace_Vector*, double);
-        EVSpace_Vector* (*EVSpace_Vector_divide)(const EVSpace_Vector*, double);
-        void (*EVSpace_Vector_iadd)(EVSpace_Vector*, const EVSpace_Vector*);
-        void (*EVSpace_Vector_isubtract)(EVSpace_Vector*, const EVSpace_Vector*);
-        void (*EVSpace_Vector_imultiply)(EVSpace_Vector*, double);
-        void (*EVSpace_Vector_idivide)(EVSpace_Vector*, double);
-        EVSpace_Vector* (*EVSpace_Vector_negative)(const EVSpace_Vector*);
+    /* vector number methods */
+    EVSpace_Vector* (*EVSpace_Vector_add)(const EVSpace_Vector*, const EVSpace_Vector*);
+    EVSpace_Vector* (*EVSpace_Vector_subtract)(const EVSpace_Vector*,
+        const EVSpace_Vector*);
+    EVSpace_Vector* (*EVSpace_Vector_multiply)(const EVSpace_Vector*, double);
+    EVSpace_Vector* (*EVSpace_Vector_divide)(const EVSpace_Vector*, double);
+    void (*EVSpace_Vector_iadd)(EVSpace_Vector*, const EVSpace_Vector*);
+    void (*EVSpace_Vector_isubtract)(EVSpace_Vector*, const EVSpace_Vector*);
+    void (*EVSpace_Vector_imultiply)(EVSpace_Vector*, double);
+    void (*EVSpace_Vector_idivide)(EVSpace_Vector*, double);
+    EVSpace_Vector* (*EVSpace_Vector_negative)(const EVSpace_Vector*);
 
-        /* matrix number methods */
-        EVSpace_Matrix* (*EVSpace_Matrix_add)(const EVSpace_Matrix*, const EVSpace_Matrix*);
-        EVSpace_Matrix* (*EVSpace_Matrix_subtract)(const EVSpace_Matrix*, const EVSpace_Matrix*);
-        EVSpace_Vector* (*EVSpace_Matrix_multiply_vector)(const EVSpace_Matrix*,
-            const EVSpace_Vector*);
-        EVSpace_Matrix* (*EVSpace_Matrix_multiply_matrix)(const EVSpace_Matrix*,
-            const EVSpace_Matrix*);
-        EVSpace_Matrix* (*EVSpace_Matrix_multiply_scalar)(const EVSpace_Matrix*, double);
-        EVSpace_Matrix* (*EVSpace_Matrix_divide)(const EVSpace_Matrix*, double);
-        void (*EVSpace_Matrix_iadd)(EVSpace_Matrix*, const EVSpace_Matrix*);
-        void (*EVSpace_Matrix_isubtract)(EVSpace_Matrix*, const EVSpace_Matrix*);
-        void (*EVSpace_Matrix_imultiply_scalar)(EVSpace_Matrix*, double);
-        void (*EVSpace_Matrix_idivide)(EVSpace_Matrix*, double);
-        EVSpace_Matrix* (*EVSpace_Matrix_negative)(const EVSpace_Matrix*);
+    /* matrix number methods */
+    EVSpace_Matrix* (*EVSpace_Matrix_add)(const EVSpace_Matrix*, const EVSpace_Matrix*);
+    EVSpace_Matrix* (*EVSpace_Matrix_subtract)(const EVSpace_Matrix*, const EVSpace_Matrix*);
+    EVSpace_Vector* (*EVSpace_Matrix_multiply_vector)(const EVSpace_Matrix*,
+        const EVSpace_Vector*);
+    EVSpace_Matrix* (*EVSpace_Matrix_multiply_matrix)(const EVSpace_Matrix*,
+        const EVSpace_Matrix*);
+    EVSpace_Matrix* (*EVSpace_Matrix_multiply_scalar)(const EVSpace_Matrix*, double);
+    EVSpace_Matrix* (*EVSpace_Matrix_divide)(const EVSpace_Matrix*, double);
+    void (*EVSpace_Matrix_iadd)(EVSpace_Matrix*, const EVSpace_Matrix*);
+    void (*EVSpace_Matrix_isubtract)(EVSpace_Matrix*, const EVSpace_Matrix*);
+    void (*EVSpace_Matrix_imultiply_scalar)(EVSpace_Matrix*, double);
+    void (*EVSpace_Matrix_idivide)(EVSpace_Matrix*, double);
+    EVSpace_Matrix* (*EVSpace_Matrix_negative)(const EVSpace_Matrix*);
 
-        /* vector class methods */
-        double (*EVSpace_mag)(const EVSpace_Vector*);
-        double (*EVSpace_mag_squared)(const EVSpace_Vector*);
-        void (*EVSpace_normalize)(EVSpace_Vector*);
+    /* vector class methods */
+    double (*EVSpace_mag)(const EVSpace_Vector*);
+    double (*EVSpace_mag_squared)(const EVSpace_Vector*);
+    void (*EVSpace_normalize)(EVSpace_Vector*);
 
-        /* module methods */
-        double (*EVSpace_dot)(const EVSpace_Vector*, const EVSpace_Vector*);
-        EVSpace_Vector* (*EVSpace_cross)(const EVSpace_Vector*, const EVSpace_Vector*);
-        EVSpace_Vector* (*EVSpace_norm)(const EVSpace_Vector*);
-        double (*EVSpace_vang)(const EVSpace_Vector*, const EVSpace_Vector*);
-        EVSpace_Vector* (*EVSpace_vxcl)(const EVSpace_Vector*, const EVSpace_Vector*);
-        EVSpace_Vector* (*EVSpace_proj)(const EVSpace_Vector*, const EVSpace_Vector*);
-        double (*EVSpace_det)(const EVSpace_Matrix*);
-        EVSpace_Matrix* (*EVSpace_transpose)(const EVSpace_Matrix*);
+    /* module methods */
+    double (*EVSpace_dot)(const EVSpace_Vector*, const EVSpace_Vector*);
+    EVSpace_Vector* (*EVSpace_cross)(const EVSpace_Vector*, const EVSpace_Vector*);
+    EVSpace_Vector* (*EVSpace_norm)(const EVSpace_Vector*);
+    double (*EVSpace_vang)(const EVSpace_Vector*, const EVSpace_Vector*);
+    EVSpace_Vector* (*EVSpace_vxcl)(const EVSpace_Vector*, const EVSpace_Vector*);
+    EVSpace_Vector* (*EVSpace_proj)(const EVSpace_Vector*, const EVSpace_Vector*);
+    double (*EVSpace_det)(const EVSpace_Matrix*);
+    EVSpace_Matrix* (*EVSpace_transpose)(const EVSpace_Matrix*);
 
-        /* rotation orders */
-        const EVSpace_Order* EVSpace_XYZ;
-        const EVSpace_Order* EVSpace_XZY;
-        const EVSpace_Order* EVSpace_YXZ;
-        const EVSpace_Order* EVSpace_YZX;
-        const EVSpace_Order* EVSpace_ZXY;
-        const EVSpace_Order* EVSpace_ZYX;
-        const EVSpace_Order* EVSpace_XYX;
-        const EVSpace_Order* EVSpace_XZX;
-        const EVSpace_Order* EVSpace_YXY;
-        const EVSpace_Order* EVSpace_YZY;
-        const EVSpace_Order* EVSpace_ZXZ;
-        const EVSpace_Order* EVSpace_ZYZ;
+    /* rotation orders */
+    const EVSpace_Order* EVSpace_XYZ;
+    const EVSpace_Order* EVSpace_XZY;
+    const EVSpace_Order* EVSpace_YXZ;
+    const EVSpace_Order* EVSpace_YZX;
+    const EVSpace_Order* EVSpace_ZXY;
+    const EVSpace_Order* EVSpace_ZYX;
+    const EVSpace_Order* EVSpace_XYX;
+    const EVSpace_Order* EVSpace_XZX;
+    const EVSpace_Order* EVSpace_YXY;
+    const EVSpace_Order* EVSpace_YZY;
+    const EVSpace_Order* EVSpace_ZXZ;
+    const EVSpace_Order* EVSpace_ZYZ;
 
-        /* rotation matrix methods */
-        EVSpace_Matrix* (*EVSpace_get_matrix)(EVSpace_Axis, double);
-        EVSpace_Matrix* (*EVSpace_get_euler)(const EVSpace_Order*, const EVSpace_Angles*);
-        EVSpace_Matrix* (*EVSpace_from_to)(const EVSpace_Order*, const EVSpace_Angles*,
-            const EVSpace_Order*, const EVSpace_Angles*);
+    /* rotation matrix methods */
+    EVSpace_Matrix* (*EVSpace_get_matrix)(EVSpace_Axis, double);
+    EVSpace_Matrix* (*EVSpace_get_euler)(const EVSpace_Order*, const EVSpace_Angles*);
+    EVSpace_Matrix* (*EVSpace_from_to)(const EVSpace_Order*, const EVSpace_Angles*,
+        const EVSpace_Order*, const EVSpace_Angles*);
 
-        /* rotate vector methods */
-        EVSpace_Vector* (*EVSpace_axis_to)(EVSpace_Axis, double, const EVSpace_Vector*);
-        EVSpace_Vector* (*EVSpace_axis_from)(EVSpace_Axis, double, const EVSpace_Vector*);
-        EVSpace_Vector* (*EVSpace_euler_to)(const EVSpace_Order*, const EVSpace_Angles*,
-            const EVSpace_Vector*);
-        EVSpace_Vector* (*EVSpace_euler_from)(const EVSpace_Order*, const EVSpace_Angles*,
-            const EVSpace_Vector*);
-        EVSpace_Vector* (*EVSpace_matrix_to)(const EVSpace_Matrix*, const EVSpace_Vector*);
-        EVSpace_Vector* (*EVSpace_matrix_from)(const EVSpace_Matrix*, const EVSpace_Vector*);
-        EVSpace_Vector* (*EVSpace_offset_to)(const EVSpace_Matrix*, const EVSpace_Vector*,
-            const EVSpace_Vector*);
-        EVSpace_Vector* (*EVSpace_offset_from)(const EVSpace_Matrix*, const EVSpace_Vector*,
-            const EVSpace_Vector*);
+    /* rotate vector methods */
+    EVSpace_Vector* (*EVSpace_axis_to)(EVSpace_Axis, double, const EVSpace_Vector*);
+    EVSpace_Vector* (*EVSpace_axis_from)(EVSpace_Axis, double, const EVSpace_Vector*);
+    EVSpace_Vector* (*EVSpace_euler_to)(const EVSpace_Order*, const EVSpace_Angles*,
+        const EVSpace_Vector*);
+    EVSpace_Vector* (*EVSpace_euler_from)(const EVSpace_Order*, const EVSpace_Angles*,
+        const EVSpace_Vector*);
+    EVSpace_Vector* (*EVSpace_matrix_to)(const EVSpace_Matrix*, const EVSpace_Vector*);
+    EVSpace_Vector* (*EVSpace_matrix_from)(const EVSpace_Matrix*, const EVSpace_Vector*);
+    EVSpace_Vector* (*EVSpace_offset_to)(const EVSpace_Matrix*, const EVSpace_Vector*,
+        const EVSpace_Vector*);
+    EVSpace_Vector* (*EVSpace_offset_from)(const EVSpace_Matrix*, const EVSpace_Vector*,
+        const EVSpace_Vector*);
 
-    } EVSpace_CAPI;
+} EVSpace_CAPI;
 
 #define EVSpace_CAPSULE_NAME "pyevspace.evspace_CAPI"
 
 
-    /* This is only used by the API and should not be included in
-     * evspacemodule.c since it doesn't use the capsule.
-     */
+/* This is only used by the API and should not be included in
+    * evspacemodule.c since it doesn't use the capsule.
+    */
 #ifndef _EVSPACE_IMPL
-     /* Define global variable for the C API and a macro for setting it. */
-    static EVSpace_CAPI* EVSpaceAPI = NULL;
+
+/* Define global variable for the C API and a macro for setting it. */
+static EVSpace_CAPI* EVSpaceAPI = NULL;
 
 #define EVSpace_IMPORT EVSpaceAPI \
             = (EVSpace_CAPI *)PyCapsule_Import(EVSpace_CAPSULE_NAME, 0)
@@ -259,9 +256,5 @@
 #define EVSpace_offset_from(m, o, v) = EVSpaceAPI->EVSpace_offset_from(m, o, v)
 
 #endif // !defined(_EVSPACE_IMPL)
-
-//#ifdef __cplusplus
-//}
-//#endif
 
 #endif // !defined(EVSPACE_API_H)
