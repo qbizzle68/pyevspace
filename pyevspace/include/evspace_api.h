9da67af4cad4c18118f46a26b8574353027dc2d3
qbarnes <quinton.barnes68@gmail.com>
2023-07-28 23:33:15 -0500

    added master member to EVSpace_Angles

--- pyevspace/include/evspace_api.h
+++ pyevspace/include/evspace_api.h
@@ -21,6 +21,8 @@ typedef struct {
     double alpha;
     double beta;
     double gamma;
+    // Keep track of the reference frame for callback.
+    EVSpace_Matrix* master;
 } EVSpace_Angles;
 
 typedef enum {
