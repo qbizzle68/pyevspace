ec9d655238e57548f37b35e66c250621eadcf48d
qbarnes <quinton.barnes68@gmail.com>
2023-07-30 02:54:44 -0500

    removed commented code

--- pyevspace/include/evspace_vector.h
+++ pyevspace/include/evspace_vector.h
@@ -201,9 +201,6 @@ vector_richcompare(EVSpace_Vector* self, PyObject* other, int op)
             else {
                 Py_RETURN_FALSE;
             }
-            //return __vector_eq(self, (EVSpace_Vector*)other)
-                //? Py_NewRef(Py_True)
-                //: Py_NewRef(Py_False);
         }
         else if (op == Py_NE)
         {
@@ -213,9 +210,6 @@ vector_richcompare(EVSpace_Vector* self, PyObject* other, int op)
             else {
                 Py_RETURN_FALSE;
             }
-            /*return (!__vector_eq(self, (EVSpace_Vector*)other))
-                ? Py_NewRef(Py_True)
-                : Py_NewRef(Py_False);*/
         }
     }
 
