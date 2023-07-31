2f371ceabae96aa8872ad536055af4b9842cadfa
Quinton Barnes <95594823+qbizzle68@users.noreply.github.com>
2023-07-30 23:51:04 -0500

    Revert "fixed storing temp variable too late"
    This reverts commit 07739ffd94d1e232965ebe136b9f9b8f0cdd22e3.

--- pyevspace/include/evspace_angles.h
+++ pyevspace/include/evspace_angles.h
@@ -147,12 +147,12 @@ angles_setter(EVSpace_Angles* self, PyObject* rhs, void* closure)
         angle_addr = &self->gamma;
     }
 
-    // Track the original value to revert if computing matrix fails.
-    double old_value = *angle_addr;
     *angle_addr = value;
     EVSpace_ReferenceFrame* master = (EVSpace_ReferenceFrame*)self->master;
 
     if (self->master) {
+        // Track the original value to revert if computing matrix fails.
+        double old_value = *angle_addr;
         if (_refframe_update_matrix(master) < 0) {
             *angle_addr = old_value;
             return -1;
