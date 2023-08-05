dba8c97a23b879b285541e3022cadb3910d4d72b
qbarnes <quinton.barnes68@gmail.com>
2023-08-04 19:19:26 -0500

    import __version__ from importlib.metadata

--- src/pyevspace/__init__.py
+++ src/pyevspace/__init__.py
@@ -1,8 +1,11 @@
 """A Python 3-dimensional Euclidean vector space."""
 
+import importlib.metadata
+
+__version__ = importlib.metadata.version(__package__)
+
 try:
     from .core import *
 except ModuleNotFoundError:
+    # todo: import pure python version here
     pass
-
-__version__ = '0.14.1'
